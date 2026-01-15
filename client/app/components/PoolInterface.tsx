a0dfda81c957ab6c11cbb929d63abc33159f6d0c
Ashley-code396 <ashleymwende24@gmail.com>
2026-01-15 19:47:22 +0300

    Format

--- client/app/components/PoolInterface.tsx
+++ client/app/components/PoolInterface.tsx
@@ -12,6 +12,7 @@ import {
 import { SuiObjectResponse, SuiObjectData } from "@mysten/sui/client";
 import { TESTNET_PACKAGE_ID } from "../constants";
 
+
 type PoolSummary = {
   id: string;
   token1: string;
