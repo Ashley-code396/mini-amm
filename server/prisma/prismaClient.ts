40420bd1f2b908676d506a103ee75ea34ae1016b
Ashley-code396 <ashleymwende24@gmail.com>
2026-01-17 22:29:16 +0300

    Generate prisma client

--- server/prisma/prismaClient.ts
+++ server/prisma/prismaClient.ts
@@ -1,15 +1,15 @@
 
-// import "dotenv/config";
-// import { PrismaPg } from '@prisma/adapter-pg';
-// import { PrismaClient } from '../generated/prisma/client';
+import "dotenv/config";
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '../generated/prisma/client';
 
-// const connectionString = process.env.DATABASE_URL;
+const connectionString = process.env.DATABASE_URL;
 
-// if (!connectionString) {
-//   throw new Error("DATABASE_URL is not defined in your environment");
-// }
+if (!connectionString) {
+  throw new Error("DATABASE_URL is not defined in your environment");
+}
 
-// const adapter = new PrismaPg({ connectionString });
-// const prisma = new PrismaClient({ adapter });
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
 
-// export { prisma };
+export { prisma };
